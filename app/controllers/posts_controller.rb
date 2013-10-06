4428f9419a440cf3bb9d0ab0dbc3ae36663a5bc0
Mark Horowitz <msh345+lists@gmail.com>
2013-10-05 22:03:52 -0500

    fixed new method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,6 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
-	def new
-	end
-
 	def create
 		# render text: params[:post].inspect
 		@post = Post.new(params[:post].permit(:title,:text))
@@ -17,9 +14,36 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+	  @post = Post.find(params[:id])
+	  @post.destroy
+	 
+	  redirect_to posts_path
+	end
+
+
 
 	private
 		def post_params
