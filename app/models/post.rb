0a0777a9e9574a2c050167cc13a464fa8484d71b
Mark Horowitz <msh345+lists@gmail.com>
2013-10-06 09:51:09 -0500

    done with 8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	include ActiveModel::ForbiddenAttributesProtection
   attr_accessible :text, :title
