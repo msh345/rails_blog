8390c5e895b8b230c1ca068bc1fd5a249cf0ea2f
Mark Horowitz <msh345+lists@gmail.com>
2013-10-06 09:41:25 -0500

    done with 6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments
 	validates :title, presence: true, length: { minimum: 5 }
 	include ActiveModel::ForbiddenAttributesProtection
   attr_accessible :text, :title
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RailsBlog::Application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
 
   # The priority is based upon order of creation:
